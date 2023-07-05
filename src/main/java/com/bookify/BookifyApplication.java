30596e8d35c6b9682affa563d83256224ad04909
Alex Kostas <k.alexandros299@gmail.com>
2023-07-05 16:36:13 +0300

    Fixed issue

--- src/main/java/com/bookify/BookifyApplication.java
+++ src/main/java/com/bookify/BookifyApplication.java
@@ -8,7 +8,7 @@ public class BookifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookifyApplication.class, args);
-
+		System.out.println("Hello World!");
 	}
 
 }
