d3640c817b691cca2755d8d7c4de47ef53c74c5c
Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
2023-07-05 16:15:24 +0300

    test

--- src/main/java/com/bookify/BookifyApplication.java
+++ src/main/java/com/bookify/BookifyApplication.java
@@ -2,8 +2,6 @@ package com.bookify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.autoconfigure.jdbc.*;
 
 @SpringBootApplication
 public class BookifyApplication {
