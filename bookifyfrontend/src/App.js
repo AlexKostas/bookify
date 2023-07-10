8e058a3e63b3b942982078da27aa391d6bdfebe9
Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
2023-07-11 00:18:51 +0300

    sh!t's too much, retrieved to tutorials... just comitting in order to checkout other branch for merging (improvements)

--- bookifyfrontend/src/App.js
+++ bookifyfrontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import CustomAppBar from './components/welcomepage/AppBar';
-import SelectTextFields from './components/welcomepage/SearchBar';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import CustomAppBar from './components/CustomAppBar';
+import Login from './components/auth/Login';
+import Registration from './components/auth/Registration';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
+        <div>
             <CustomAppBar />
-            {/* Other components and content */}
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Registration} />
+            </Switch>
         </div>
     );
-}
+};
 
 export default App;
-
--- bookifyfrontend/src/index.js
+++ bookifyfrontend/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {AuthProvider} from "react-auth-kit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={true}
+    >
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
