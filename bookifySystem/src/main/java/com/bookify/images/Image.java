d0ca08b75bb6ccbb082b0d2817a4d30d6be699c6
Alex Kostas <k.alexandros299@gmail.com>
2023-07-12 01:32:52 +0300

    Small fixes

--- bookifySystem/src/main/java/com/bookify/images/Image.java
+++ bookifySystem/src/main/java/com/bookify/images/Image.java
@@ -1,7 +1,9 @@
 package com.bookify.images;
 
-import com.bookify.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
