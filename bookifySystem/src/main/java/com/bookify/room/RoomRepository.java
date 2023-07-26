2836971506afc9ff00e55c87838a7b54f7bfd1d8
AlexKostas <k.alexandros299@gmail.com>
2023-07-26 18:03:53 +0300

    Preliminary filter based room search

--- bookifySystem/src/main/java/com/bookify/room/RoomRepository.java
+++ bookifySystem/src/main/java/com/bookify/room/RoomRepository.java
@@ -4,12 +4,23 @@ import com.bookify.room_amenities.Amenity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
     Page<Room> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM Room r " +
+            "JOIN r.amenities a " +
+            "WHERE a IN :amenities " +
+            "GROUP BY r " +
+            "HAVING COUNT(a) = :amenitiesCount")
+    List<Room> filterRoomsByAmenities(@Param("amenities") Set<Amenity> amenities,
+                                       @Param("amenitiesCount") Integer amenitiesCount);
 }
