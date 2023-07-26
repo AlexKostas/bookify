68aaa2a5995ef5ba1f2e3784acf5ec328af6bb52
AlexKostas <k.alexandros299@gmail.com>
2023-07-26 19:06:34 +0300

    Test

--- bookifyfrontend/src/index.js
+++ bookifyfrontend/src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios"
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
+
+axios.defaults.baseURL = 'https://localhost:8443'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
