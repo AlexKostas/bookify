d3037fcec53607f1a34edefa2f1c999da0b946e0
AlexKostas <k.alexandros299@gmail.com>
2023-08-05 04:57:00 +0300

    A proper message is shown when the room grid is empty

--- bookifyfrontend/src/components/RoomGrid/RoomGrid.jsx
+++ bookifyfrontend/src/components/RoomGrid/RoomGrid.jsx
@@ -28,17 +28,20 @@ const RoomGrid = ({endpointURL}) => {
 
   return (
     <>
-      <FilterPanel />
-      <PaginationControls onPageChanged={onPageChanged} totalPages={totalPages}>
-        <div className="room-grid">
-            {
-                rooms.map(room => (<RoomCard key={room.roomID} room={room} />))
-            }
-        </div>
-      </PaginationControls>
-      
-
-  
+      {
+        rooms.length === 0 ? <h3>No rooms found</h3> : (
+          <div>
+            <FilterPanel />
+            <PaginationControls onPageChanged={onPageChanged} totalPages={totalPages}>
+              <div className="room-grid">
+                  {
+                      rooms.map(room => (<RoomCard key={room.roomID} room={room} />))
+                  }
+              </div>
+            </PaginationControls>
+          </div>
+        )
+      }
     </>
   );
 }
