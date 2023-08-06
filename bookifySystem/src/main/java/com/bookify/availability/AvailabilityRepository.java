12d42fdce6efcb716e2d54306110d8572c744a92
AlexKostas <k.alexandros299@gmail.com>
2023-08-06 16:39:47 +0300

    Created entity and repository for availability

---
+++ bookifySystem/src/main/java/com/bookify/availability/AvailabilityRepository.java
@@ -0,0 +1,8 @@
+package com.bookify.availability;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+}
