5d050bc9fcb5faffb652d531dcc2e8fc8bfd9659
AlexKostas <k.alexandros299@gmail.com>
2023-08-10 17:26:52 +0300

    More styling changes

--- bookifyfrontend/src/components/LoginForm/LoginForm.jsx
+++ bookifyfrontend/src/components/LoginForm/LoginForm.jsx
@@ -1,14 +1,14 @@
 import { useRef, useState, useEffect } from 'react';
-import useAuth from '../hooks/useAuth';
+import useAuth from '../../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import axios from '../api/axios';
-import { useLocalStorage } from '../hooks/useLocalStorage';
-import { red } from '@material-ui/core/colors';
+import axios from '../../api/axios';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import './loginform.css';
 
 // The login api endpoint URL
 const LOGIN_URL = '/registration/login';
 
-const Login = () => {
+const LoginForm = () => {
     const { setAuth } = useAuth();
     const { setItem } = useLocalStorage();
 
@@ -85,41 +85,42 @@ const Login = () => {
     }
 
     return (
+        <div className='container'>
+            <div className='login-form'>
+                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        required
+                    />
 
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
-                    required
-                />
-
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                />
-                <button>Sign In</button>
-            </form>
-            <p>
-                Need an Account?<br />
-                <span className="line">
-                    <Link to="/register">Sign Up</Link>
-                </span>
-            </p>
-        </section>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
+                    <button>Sign In</button>
+                </form>
+                <p>
+                    Need an Account?<br />
+                    <span className="line">
+                        <Link to="/register">Sign Up</Link>
+                    </span>
+                </p>
+            </div>
+        </div>
 
     )
 }
 
-export default Login
+export default LoginForm
