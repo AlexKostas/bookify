6359ab162326fe7ef80a56b4390f8a8586648136
Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
2023-08-13 15:54:13 +0300

    Edit profile pic done (i think)

--- bookifyfrontend/src/components/UserView/UserView.jsx
+++ bookifyfrontend/src/components/UserView/UserView.jsx
@@ -1,21 +1,25 @@
 import useImageFetcher from "../../hooks/useImageFetcher";
 import { CircularProgress } from "@mui/material";
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import './userview.css';
 import UserDetails from "../UserDetails/UserDetails";
 import useAuth from '../../hooks/useAuth';
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const UserView = ({ username }) => {
     const profilePicURL = `/upload/getProfilePic/${username}`;
     const editProfilePicUrl = `/upload/uploadProfilePic/${username}`;
 
     const [error, setError] = useState('');
-    const {imageData, loading} = useImageFetcher(profilePicURL);
+    const [success, setSuccess] = useState(null);
+    const clearError = () => setError(null);
+
+    const { imageData, loading, setImageData } = useImageFetcher(profilePicURL);
     const [tabValue, setTabValue] = useState(0);
-    const [profileImage, setProfileImage] = useState('default-profile-image.jpg');
 
     const { auth } = useAuth();
     const axiosPrivate = useAxiosPrivate();
@@ -27,7 +31,7 @@ const UserView = ({ username }) => {
         inputRef.current.click();
     };
 
-    const handleImageChange = (event) => {
+    const handleImageChange = async (event) => {
         const selectedImage = event.target.files && event.target.files[0];
         event.target.value = null;
         if (!selectedImage) {
@@ -36,36 +40,56 @@ const UserView = ({ username }) => {
         if (selectedImage) {
             const formData = new FormData();
             formData.append('file', selectedImage);
-            const imageURL = URL.createObjectURL(selectedImage);
-            setProfileImage(imageURL);
 
             try {
-                const response = axiosPrivate.post(editProfilePicUrl, formData, {
+                const response = await  axiosPrivate.post(editProfilePicUrl, formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
                 });
+                setSuccess("Profile picture updated successfully!");
+                setImageData(URL.createObjectURL(selectedImage));
+                setError(null);
+                setTimeout(() => {setSuccess(null)}, 5000);
             }
             catch(err) {
-                let errorMessage = '';
-                if (!err?.response)
-                    errorMessage = 'No Server Response';
-                else if (err.response?.status === 400)
-                    errorMessage = 'Illegal argument';
-                else if (err.response?.status === 500)
-                    errorMessage = 'Internal server error';
-                else
-                    errorMessage = 'Edit profile picture Failed'
-
-                setError(errorMessage)
                 console.log(err);
+                if (!err?.response)
+                    setError('No Server Response');
+                else if (err.response?.status === 400)
+                    setError('Only “jpg” and “png” image file formats are accepted');
+                else if (err.response?.status === 500)
+                    setError('Internal server error');
+                else
+                    setError('Edit profile picture Failed');
+
+                setSuccess(null);
+                setTimeout(clearError, 5000);
             }
-            // URL.revokeObjectURL(imageURL)
         }
     };
 
+    useEffect(() => {
+        return () => {
+            if (imageData) {
+                URL.revokeObjectURL(imageData);
+            }
+        };
+    }, [imageData]);
+
     return (
-    <>    
+    <>
+        {success ? (
+            <Alert severity="success" onClose={() => { setSuccess(null) }}>
+                <AlertTitle>Success</AlertTitle>
+                {success}
+            </Alert>
+        ) : error && (
+            <Alert severity="error" onClose={clearError}>
+                <AlertTitle>Error</AlertTitle>
+                {error}
+            </Alert>
+        )}
         <div className='container'>
             {loading ? (
             <div className='loading-container'>
@@ -73,21 +97,25 @@ const UserView = ({ username }) => {
                 <h3>Loading profile picture...</h3>
             </div>
             ) : imageData ? (
-            <div className='profile-picture-container'>
-                <img onClick = {handleClick}
-                    className="profile-pic"
-                    src={imageData}
-                    alt="Could not load profile picture"
-                />
-                <input
-                    style={{display: 'none'}}
-                    ref={inputRef}
-                    type="file"
-                    onChange={handleImageChange}
-                />
-
-                <button onClick={handleClick}>Open file upload box</button>
-            </div>
+                <div className='profile-picture-container'>
+                    <button onClick={handleClick}
+                            className={"button"}
+                            disabled = {!valid}
+                    >
+                        <img
+                            className="profile-pic"
+                            src={imageData}
+                            alt="Could not load profile picture"
+                        />
+                        <input
+                            style={{display: 'none'}}
+                            ref={inputRef}
+                            type="file"
+                            accept=".png, .jpg"
+                            onChange={handleImageChange}
+                        />
+                    </button>
+                </div>
             ) : (
             <p>Could not load profile picture</p>
             )}
