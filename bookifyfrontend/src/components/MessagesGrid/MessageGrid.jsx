c247bc4434783ee5b03608efa37ee7f90d2321dc
Alex Kostas <k.alexandros299@gmail.com>
2023-08-16 20:20:58 +0300

    Test commit

--- bookifyfrontend/src/components/MessagesGrid/MessageGrid.jsx
+++ bookifyfrontend/src/components/MessagesGrid/MessageGrid.jsx
@@ -37,7 +37,7 @@ const MessageGrid = () => {
   }
 
   const checkForNewMessages = () => {
-    console.log("test");
+    console.log("test");//
   }
 
   const handlePageChange = (event, newPage) => {
