f1ed17d17cdcf58ecc8e0a7f39e9af52d2ed0dcb
Alex Kostas <k.alexandros299@gmail.com>
2023-08-16 04:29:34 +0300

    Added forgotten file to previous commit

--- bookifySystem/src/main/java/com/bookify/search/SearchPreviewDTO.java
+++ bookifySystem/src/main/java/com/bookify/search/SearchPreviewDTO.java
@@ -2,4 +2,4 @@ package com.bookify.search;
 
 //TODO: add thumbnail guid, cost per day, room type
 public record SearchPreviewDTO(int roomID, String name, float rating, int reviewCount,
-                               int bedCount, int price, String roomType, String thumbnail) {}
+                               int bedCount, float price, String roomType, String thumbnail) {}
