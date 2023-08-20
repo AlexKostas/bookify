a9eae73d0a8aa4b472736fab14c46d95ed769ade
Alex Kostas <k.alexandros299@gmail.com>
2023-08-20 22:37:52 +0300

    Basic room page structure

--- bookifySystem/src/main/java/com/bookify/reviews/ReviewRepository.java
+++ bookifySystem/src/main/java/com/bookify/reviews/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.bookify.reviews;
 import com.bookify.room.Room;
 import com.bookify.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
---
+++ bookifyfrontend/src/components/CreateRoom/CreateRoom.jsx
@@ -0,0 +1,282 @@
+import './createRoom.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {Button, CircularProgress} from "@mui/material";
+import {useEffect, useState} from "react";
+import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import DialogContent from "@mui/material/DialogContent";
+import Dialog from "@mui/material/Dialog";
+import CheckboxSelection from "../CheckboxSelection/CheckboxSelection";
+import axios from "../../api/axios";
+import useAuth from "../../hooks/useAuth";
+
+const CreateRoom = ({ roomID }) => {
+    const [room, setRoom] = useState(null);
+    const [newRoom, setNewRoom] = useState({});
+    const [amenitiesActive, setAmenitiesActive] = useState(false);
+    const [roomTypeActive, setRoomTypeActive] = useState(false);
+    const [unauthenticated, setUnauthenticated] = useState(false);
+    const [tempAmenities, setTempAmenities] = useState([]);
+    const [tempRoomTypeID, setTempRoomTypeID] = useState(-1);
+
+    const { auth } = useAuth();
+
+    const preloadRoomInfo = () => {
+        if(room.hostUsername !== auth?.user) setUnauthenticated(true);
+    }
+
+    const onSubmit = () => {
+        if(unauthenticated) return;
+    }
+
+    const fetchRoom = async () => {
+        try {
+            const response = await axios.get(`/room/getRoom/${roomID}`);
+            setRoom(response?.data ?? null);
+        } catch (error) {
+            console.log(error);
+            setRoom(null);
+        }
+    }
+
+    useEffect(() => {
+        if(roomID) fetchRoom(roomID);
+    }, [roomID]);
+
+    useEffect(() => {
+        if(room) preloadRoomInfo();
+    }, [room]);
+
+    //TO BE DELETED:
+    useEffect(() => {
+        console.log(newRoom);
+    }, [newRoom]);
+
+    return (
+        <div className="create-room-parent">
+            <div className="create-room-content">
+                    <h1>New Room</h1>
+
+                    <div className="accordion-parent">
+
+                        <Accordion defaultExpanded={true}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>General Info</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <div>
+
+                                </div>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Space</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <div>
+                                    <Button
+                                        onClick={() => setAmenitiesActive(true)}
+                                    >
+                                        Select amenities
+                                    </Button>
+
+                                    <Button
+                                        onClick={() => setRoomTypeActive(true)}
+                                    >
+                                        Select room type
+                                    </Button>
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Accordion 1</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Accordion 1</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Accordion 1</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+
+                    </div>
+                </div>
+
+            <Dialog
+                open={amenitiesActive}
+                onClose={() => setAmenitiesActive(false)}
+                maxWidth="lg"
+            >
+                    <DialogTitle
+                        style={
+                            {
+                                width: '100%',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                            } }
+                    >
+                        Select Amenities
+
+                        <IconButton
+                            edge="end"
+                            color="inherit"
+                            onClick={() => setAmenitiesActive(false)}
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    </DialogTitle>
+
+                    <DialogContent className="amenities-popup">
+
+                        <CheckboxSelection
+                            title="Amenities"
+                            endpointURL="/amenities/getAllAmenities"
+                            onFiltersChanged={(newAmenities) => {
+                                setTempAmenities(newAmenities);
+                            }}
+                            defaultData={newRoom?.amenityIDs}
+                        />
+
+
+                        <div className="amenity-popup-button-holder">
+                            <button
+                                onClick={() => {
+                                    setNewRoom({...newRoom, amenityIDs: tempAmenities});
+                                    setAmenitiesActive(false);
+                                }}
+                                className="confirm-amenities-button"
+                            >
+                                Confirm
+                            </button>
+                        </div>
+
+
+
+
+                    </DialogContent>
+
+
+            </Dialog>
+
+            <Dialog
+                open={roomTypeActive}
+                onClose={() => setRoomTypeActive(false)}
+                maxWidth="lg"
+            >
+                <DialogTitle
+                    style={
+                        {
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        } }
+                >
+                    Select Room Type
+
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        onClick={() => setRoomTypeActive(false)}
+                        aria-label="close"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </DialogTitle>
+
+                <DialogContent className="amenities-popup">
+
+                    <CheckboxSelection
+                        title="Room Type"
+                        endpointURL="/roomType/getAll"
+                        onFiltersChanged={(newRoomType) => {
+                            setTempRoomTypeID(newRoomType[0]);
+                        }}
+                        defaultData={newRoom.roomTypeID ? [newRoom.roomTypeID] : undefined}
+                        onlyOneOption={true}
+                    />
+
+
+                    <div className="amenity-popup-button-holder">
+                        <button
+                            onClick={() => {
+                                setNewRoom({...newRoom, roomTypeID: tempRoomTypeID});
+                                setRoomTypeActive(false);
+                            }}
+                            className="confirm-amenities-button"
+                        >
+                            Confirm
+                        </button>
+                    </div>
+
+
+
+
+                </DialogContent>
+
+            </Dialog>
+
+        </div>
+    )
+};
+
+export default CreateRoom;
