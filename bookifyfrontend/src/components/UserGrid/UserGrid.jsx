6ac4e161dedaa2f6a501475e2a04d8ef182a75b2
Alex Kostas <k.alexandros299@gmail.com>
2023-08-23 23:38:48 +0300

    Fixed export buttons

--- bookifyfrontend/src/components/UserGrid/UserGrid.jsx
+++ bookifyfrontend/src/components/UserGrid/UserGrid.jsx
@@ -249,6 +249,7 @@ const UserGrid = () => {
                     color="primary"
                     onClick={() => handleExport(`admin/getHostRev${selectedFileType}`)}
                     startIcon={<CloudDownloadIcon />}
+                    disabled={fileLoading}
                 >
                     Export Host Reviews
                 </Button>
