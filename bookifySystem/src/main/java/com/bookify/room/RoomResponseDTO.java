5d2359b84ee65f7a10b61c43864bfeaa689c91ec
Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
2023-08-23 18:02:25 +0300

    Squashed commit of the following:
    commit 772728a7fb44bb9ee867a96c90d4fb2916687e64
    Merge: ceccd9c 259022e
    Author: Alex Kostas <k.alexandros299@gmail.com>
    Date:   Mon Aug 21 19:53:27 2023 +0300
    
        Merge pull request #29
    
        Several stuff
    
    commit 259022ecdc9e28ff4b0591039d6e1d4b47e1914b
    Author: Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
    Date:   Mon Aug 21 19:51:01 2023 +0300
    
        Changed done accordingly.
    
    commit 3d48ab72a88f966a95e388db52f0406f6228f57a
    Author: Konstantinos Arkoulis <arkoulis.kostas02@gmail.com>
    Date:   Mon Aug 21 19:36:19 2023 +0300
    
        Several small changes
    
    commit ceccd9c1afa5ea1322538d4f2a834068ea25928c
    Merge: b1a0c5e 57b2f7a
    Author: Alex Kostas <k.alexandros299@gmail.com>
    Date:   Sun Aug 20 23:21:25 2023 +0300
    
        Merge pull request #27
    
        Host Reviews feature added for admin
    
    commit 57b2f7a205049fc874cce46df603966e55dc7765
    Author: arkou <arkoulis.kostas02@gmail.com>
    Date:   Sun Aug 20 23:12:53 2023 +0300
    
        Host Reviews files feature for admin added.
    
    commit f23297a8ad408682e02548359babec8161c7460e
    Author: arkou <arkoulis.kostas02@gmail.com>
    Date:   Sun Aug 20 21:09:29 2023 +0300
    
        Booking tested -- Passed!

--- bookifySystem/src/main/java/com/bookify/room/RoomResponseDTO.java
+++ bookifySystem/src/main/java/com/bookify/room/RoomResponseDTO.java
@@ -8,7 +8,7 @@ public record RoomResponseDTO (
         String transitInfo,         String city,         String state,                  String country,                 String zipcode,
         String latitude,            String longitude,    int minimumStay,               String rules,
         int nBeds,                  int nBaths,          int nBedrooms,                 int surfaceArea,                int accommodates,
-        String roomType,            float pricePerNight, int maxTenants,                float extraCostPerTenant,       int roomTypeID,
+        String roomType,            float pricePerNight, int maxTenants,                float extraCostPerTenant,
         List<String> amenityNames,  List<String> amenityDescriptions,                   List<Integer> amenityIDs,
         String thumbnailGuid,       List<String> photosGUIDs,                           float rating,                   int reviewCount
 ) {}
