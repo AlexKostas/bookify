12d954dd219c0ecbddc9705d1b78cbbb503f9759
Alex Kostas <k.alexandros299@gmail.com>
2023-08-24 18:53:59 +0300

    Added recommendation logic

--- bookifySystem/src/main/java/com/bookify/recommendation/RecommendationService.java
+++ bookifySystem/src/main/java/com/bookify/recommendation/RecommendationService.java
@@ -10,8 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
